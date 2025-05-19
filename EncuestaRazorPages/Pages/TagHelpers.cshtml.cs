ca925f17906bc75bf9c73795bd14dfa100b0515f
Alex <acst352@gmail.com>
2025-05-19 13:58:52 -0600

    Select tag helper

--- EncuestaRazorPages/Pages/TagHelpers.cshtml.cs
+++ EncuestaRazorPages/Pages/TagHelpers.cshtml.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EncuestaRazorPages.Pages
 {
@@ -10,6 +11,14 @@ namespace EncuestaRazorPages.Pages
         [Required]
         [Display(Name = "Correo Electronico")]
         public string CorreoElectronico { get; set; }
+
+        public List<SelectListItem> Numeros = Enumerable.Range(1, 10).Select(n => new SelectListItem
+        {
+            Value = n.ToString(),
+            Text = n.ToString()
+        }).ToList();
+
+        public int NumeroSeleccionado { get; set; } = 7;
         public void OnGet()
         {
         }
