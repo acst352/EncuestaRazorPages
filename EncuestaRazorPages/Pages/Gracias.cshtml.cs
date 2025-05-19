e28c766124e03bd922289e0e5ca1b79b38a86f78
Alex <acst352@gmail.com>
2025-05-19 17:06:53 -0600

    Add Encuesta, Gracias, Resultados pages

---
+++ EncuestaRazorPages/Pages/Gracias.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EncuestaRazorPages.Pages
+{
+    public class GraciasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EncuestaRazorPages/Pages/Resultados.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EncuestaRazorPages.Pages
+{
+    public class ResultadosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
