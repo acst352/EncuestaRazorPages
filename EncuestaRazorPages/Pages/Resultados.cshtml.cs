e7fe3ebb7895c67359b816957901492cb4105ecf
Alex <acst352@gmail.com>
2025-05-20 11:45:25 -0600

    Add the other views

--- EncuestaRazorPages/Pages/Resultados.cshtml.cs
+++ EncuestaRazorPages/Pages/Resultados.cshtml.cs
@@ -1,3 +1,5 @@
+using EncuestaRazorPages.Models;
+using EncuestaRazorPages.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,17 @@ namespace EncuestaRazorPages.Pages
 {
     public class ResultadosModel : PageModel
     {
+        private readonly ServicioEncuestas _servicioEncuestas;
+
+        public ResultadosModel(ServicioEncuestas servicioEncuestas)
+        {
+            _servicioEncuestas = servicioEncuestas;
+        }
+
+        public List<Encuesta> Encuestas { get; set; }
         public void OnGet()
         {
+            Encuestas = _servicioEncuestas.Encuestas.ToList();
         }
     }
 }
