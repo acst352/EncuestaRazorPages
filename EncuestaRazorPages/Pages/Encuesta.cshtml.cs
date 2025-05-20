f9c343613af49f975c3746ac6b7ff84478811333
Alex <acst352@gmail.com>
2025-05-20 11:21:08 -0600

    Adding logic to Encuesta

--- EncuestaRazorPages/Pages/Encuesta.cshtml.cs
+++ EncuestaRazorPages/Pages/Encuesta.cshtml.cs
@@ -3,6 +3,7 @@ using EncuestaRazorPages.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+// Técnica de inyección de dependencias por constructor para tener acceso al servicio Encuesta
 namespace EncuestaRazorPages.Pages
 {
     public class EncuestaModel : PageModel
@@ -12,15 +13,20 @@ namespace EncuestaRazorPages.Pages
         {
             _servicioEncuesta = servicioEncuesta;
         }
-        [BindProperty]
+        // Declaración de Objeto o propiedad Encuesta
+        [BindProperty] // Permite enlazar la propiedad al enviarla al servidor mediante el método OnPost.
+                       // Permite el binding automático de los datos del formulario a la propiedad Encuesta durante el POST.
         public Encuesta Encuesta { get; set; }
+
+        // El Manejador OnGet muestra la página
         public void OnGet()
         {
         }
-
+        // El método OnPost guarda una nueva encuesta
         public IActionResult OnPost()
         {
             _servicioEncuesta.Agregar(Encuesta);
+            // Redireccionar a página Gracias
             return RedirectToPage("Gracias");
         }
     }
--- EncuestaRazorPages/Servicios/ServicioEncuestas.cs
+++ EncuestaRazorPages/Servicios/ServicioEncuestas.cs
@@ -4,10 +4,11 @@ namespace EncuestaRazorPages.Servicios
 {
     public class ServicioEncuestas
     {
-        public List<Encuesta> Encuestas { get; set; }
-        public void Agregar(Encuesta encuesta)
+        public List<Encuesta> Encuestas { get; set; } = new List<Encuesta>();
+
+        internal void Agregar(Encuesta encuesta)
         {
-            Encuestas.Add(encuesta);
+            throw new NotImplementedException();
         }
     }
 }
