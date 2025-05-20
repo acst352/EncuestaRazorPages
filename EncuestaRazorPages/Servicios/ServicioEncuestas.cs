21c97b20f4ed718e4f30d205a458eb22e86a7919
Alex <acst352@gmail.com>
2025-05-20 11:51:30 -0600

    Add comments

--- EncuestaRazorPages/Servicios/ServicioEncuestas.cs
+++ EncuestaRazorPages/Servicios/ServicioEncuestas.cs
@@ -5,6 +5,9 @@ namespace EncuestaRazorPages.Servicios
     public class ServicioEncuestas
     {
         public List<Encuesta> Encuestas { get; set; } = new List<Encuesta>();
+        // Inicializa la lista Encuestas al crear una instancia de ServicioEncuestas.
+        // Siempre habrá una lista válida (no nula) para agregar elementos.
+        // El método Agregar nunca lanzará excepciones por referencia nula.
 
         internal void Agregar(Encuesta encuesta)
         {
